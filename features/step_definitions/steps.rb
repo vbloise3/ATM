3c62b8777babf1e117c7b97dcea54e9363bf0616
Vince Bloise <vbloise3@verizon.net>
2014-12-31 18:26:30 -0500

    Cleaned up transform to capture cash

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -8,14 +8,14 @@ class Account
   end
 end
 
-Transform /^\d+$/ do |number|
-  number.to_i
+CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
+  digits.to_i
 end
 
-Given /^I have deposited \$(\d+) in my account$/ do |amount|
+Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
   my_account = Account.new
-  my_account.deposit(amount.to_i)
-  expect(my_account.balance).to eq(amount.to_i), "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  my_account.deposit(amount)
+  expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When /^I request \$(\d+)$/ do |arg1|

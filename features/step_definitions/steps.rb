f7fcff2bc7989d3a0a2860df051eb69ccd7c4bab
Vince Bloise <vbloise3@verizon.net>
2014-12-31 18:29:42 -0500

    Test to see if Jenkins is seeing changes

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -12,7 +12,7 @@ CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
   digits.to_i
 end
 
-Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
+Given /^I have deposited (#{CAPTURE_CASH_AMOUNTS}) in my account$/ do |amount|
   my_account = Account.new
   my_account.deposit(amount)
   expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but it was #{my_account.balance}"
